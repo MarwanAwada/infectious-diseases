3da308d0b0e57d389f3c7d2ef4ad11378fabf8e5
MarwanAwada <45666666+MarwanAwada@users.noreply.github.com>
2022-05-04 23:10:49 +0300

    Add files via upload

--- Infectious Diseases.js
+++ Infectious Diseases.js
@@ -12,13 +12,27 @@ function Fever() {
 		    && !document.getElementById("Intoxication.1").checked
 		    && !document.getElementById("Intoxication.2").checked
 		    && !document.getElementById("Intoxication.3").checked)
+		|| (!document.getElementById("Defecation.0").checked
+			&& !document.getElementById("Defecation.1").checked
+			&& !document.getElementById("Defecation.2").checked
+			&& !document.getElementById("Defecation.3").checked
+			&& !document.getElementById("Defecation.4").checked)
+		|| (!document.getElementById("Color.0").checked
+			&& !document.getElementById("Color.1").checked
+			&& !document.getElementById("Color.2").checked
+			&& !document.getElementById("Color.3").checked)
+		|| (!document.getElementById("Odour.0").checked
+			&& !document.getElementById("Odour.1").checked
+			&& !document.getElementById("Odour.2").checked)
+		|| (!document.getElementById("Impurities.0").checked
+			&& !document.getElementById("Impurities.1").checked
+			&& !document.getElementById("Impurities.2").checked)
 		|| (!document.getElementById("Nausea.0").checked
 		    && !document.getElementById("Nausea.1").checked)
 		|| (!document.getElementById("Stool.0").checked
 			&& !document.getElementById("Stool.1").checked
 			&& !document.getElementById("Stool.2").checked
-			&& !document.getElementById("Stool.3").checked
-			&& !document.getElementById("Stool.4").checked)
+			&& !document.getElementById("Stool.3").checked)
 		|| (!document.getElementById("Vomiting.0").checked
 		    && !document.getElementById("Vomiting.1").checked
 		    && !document.getElementById("Vomiting.2").checked
@@ -29,6 +43,8 @@ function Fever() {
 			&& !document.getElementById("Abdominal.pain.1").checked
 			&& !document.getElementById("Abdominal.pain.2").checked
 			&& !document.getElementById("Abdominal.pain.3").checked)
+		|| (!document.getElementById("Tenesmus.0").checked
+		    && !document.getElementById("Tenesmus.1").checked)
 		|| (!document.getElementById("Diarrhea.0").checked
 			&& !document.getElementById("Diarrhea.1").checked) 
 		|| (!document.getElementById("Dehydration.0").checked
@@ -42,11 +58,19 @@ function Fever() {
 	else{
 	if (document.getElementById("Fever.2").checked 
 		&& document.getElementById("Intoxication.2").checked 
-		&& document.getElementById("Stool.0").checked 
-		&& document.getElementById("Nausea.1").checked 
-		&& document.getElementById("Vomiting.5").checked 
+		&& (document.getElementById("Defecation.2").checked
+		|| document.getElementById("Defecation.3").checked)
+		&& document.getElementById("Color.1").checked
+		&& document.getElementById("Odour.1").checked
+		&& document.getElementById("Impurities.0").checked
+		&& document.getElementById("Stool.1").checked 
+		&& document.getElementById("Nausea.1").checked
+		&& (document.getElementById("Vomiting.3").checked 
+		|| document.getElementById("Vomiting.4").checked 
+		|| document.getElementById("Vomiting.5").checked)
 		&& document.getElementById("Abdominal.pain.1").checked 
 		&& document.getElementById("Diarrhea.1").checked
+		&& document.getElementById("Tenesmus.0").checked
 		&& (document.getElementById("Dehydration.0").checked
 		|| document.getElementById("Dehydration.1").checked
 		|| document.getElementById("Dehydration.2").checked)) {
@@ -55,28 +79,46 @@ function Fever() {
 		
 	    }
 	else{
-		if (document.getElementById("Fever.1").checked 
-		    && document.getElementById("Intoxication.1").checked 
+		if ( (document.getElementById("Fever.0").checked
+			||document.getElementById("Fever.1").checked)
+		    && document.getElementById("Intoxication.1").checked
+		    && (document.getElementById("Defecation.1").checked
+		    || document.getElementById("Defecation.2").checked)
+		    && (document.getElementById("Color.0").checked
+		    || document.getElementById("Color.1").checked)
+		    && document.getElementById("Odour.0").checked
+		    && document.getElementById("Impurities.0").checked
 		    && document.getElementById("Stool.1").checked 
 		    && document.getElementById("Nausea.1").checked 
-		    && document.getElementById("Vomiting.2").checked 
+		    && document.getElementById("Vomiting.1").checked 
 		    && document.getElementById("Abdominal.pain.2").checked 
 		    && document.getElementById("Diarrhea.1").checked
+		    && document.getElementById("Tenesmus.0").checked
 		    && document.getElementById("Dehydration.0").checked) {
 		    document.getElementById("Diagnose").textContent = "На основании выбранных симптомов, Ваш предполагаемый диагноз - Норовирусная инфекция";
 		    document.getElementById("Diagnose").style.visibility = "visible"; }
 	    else {
-		    if ( (document.getElementById("Fever.3").checked || document.getElementById("Fever.2").checked)
-		        && document.getElementById("Intoxication.3").checked 
-		        && document.getElementById("Stool.2").checked 
+		    if ( (document.getElementById("Fever.2").checked
+		        || document.getElementById("Fever.3").checked) 
+		        && (document.getElementById("Intoxication.2").checked 
+		        || document.getElementById("Intoxication.3").checked)
+		        && (document.getElementById("Defecation.2").checked
+		        || document.getElementById("Defecation.3").checked)
+		        && document.getElementById("Color.0").checked
+		        && (document.getElementById("Odour.0").checked
+		        || document.getElementById("Odour.2").checked)
+		        && (document.getElementById("Impurities.1").checked
+		        || document.getElementById("Impurities.2").checked)
+		        && document.getElementById("Stool.2").checked
 		        && (document.getElementById("Nausea.0").checked
-		        	|| document.getElementById("Nausea.1").checked) 
+		        || document.getElementById("Nausea.1").checked) 
 		        && !document.getElementById("Vomiting.2").checked
 		        && !document.getElementById("Vomiting.3").checked 
      	        && !document.getElementById("Vomiting.4").checked 
 	            && !document.getElementById("Vomiting.5").checked 
 		        && document.getElementById("Abdominal.pain.3").checked 
 		        && document.getElementById("Diarrhea.1").checked
+		        && document.getElementById("Tenesmus.1").checked
 		        && document.getElementById("Dehydration.0").checked) {
 		        document.getElementById("Diagnose").textContent = "На основании выбранных симптомов, Ваш предполагаемый диагноз - Шигеллёз";
 		        document.getElementById("Diagnose").style.visibility = "visible"; }
@@ -84,28 +126,45 @@ function Fever() {
 		        if (document.getElementById("Intoxication.3").checked
 			        && !document.getElementById("Fever.0").checked 
 			        && !document.getElementById("Fever.1").checked 
-		        	// && !document.getElementById("Fever.2").checked  
-	            	&& document.getElementById("Stool.3").checked 
+			        && (document.getElementById("Defecation.2").checked
+			        || document.getElementById("Defecation.3").checked)
+			        && document.getElementById("Color.2").checked
+			        && document.getElementById("Odour.0").checked
+			        && (document.getElementById("Impurities.0").checked
+		            || document.getElementById("Impurities.1").checked)
+	            	&& document.getElementById("Stool.2").checked 
 	            	&& document.getElementById("Nausea.1").checked 
 	            	&& document.getElementById("Vomiting.3").checked 
 	            	&& document.getElementById("Abdominal.pain.1").checked 
 	            	&& document.getElementById("Diarrhea.1").checked
+	            	&& document.getElementById("Tenesmus.0").checked
 	            	&& !document.getElementById("Dehydration.3").checked) {
 		            document.getElementById("Diagnose").textContent = "На основании выбранных симптомов, Ваш предполагаемый диагноз - Сальмонеллёз";
 		            document.getElementById("Diagnose").style.visibility = "visible"; }
 	            else{
 	            	if (document.getElementById("Fever.0").checked 
 	                	&& document.getElementById("Intoxication.0").checked 
-	                	&& document.getElementById("Stool.4").checked 
+	                	&& (document.getElementById("Defecation.3").checked
+	                	|| document.getElementById("Defecation.4").checked)
+	                	&& document.getElementById("Color.3").checked
+	                	&& document.getElementById("Odour.2").checked
+	                	&& document.getElementById("Impurities.0").checked
+	                	&& document.getElementById("Stool.3").checked 
 	                	&& document.getElementById("Nausea.0").checked 
 	                	&& document.getElementById("Vomiting.4").checked 
 	                	&& document.getElementById("Abdominal.pain.0").checked 
-	                	&& document.getElementById("Diarrhea.1").checked) {
+	                	&& (document.getElementById("Tenesmus.0").checked
+	                	|| document.getElementById("Tenesmus.1").checked)
+	                	&& document.getElementById("Diarrhea.1").checked)
+	                	&& (document.getElementById("Dehydration.0").checked
+	                	|| document.getElementById("Dehydration.1").checked
+	                	|| document.getElementById("Dehydration.2").checked
+	                	|| document.getElementById("Dehydration.3").checked) {
 		                document.getElementById("Diagnose").textContent = "На основании выбранных симптомов, Ваш предполагаемый диагноз - Холера"; 
 		                document.getElementById("Diagnose").style.visibility = "visible";}
 	                else{
 	                	document.getElementById("Diagnose").style.visibility = "visible";
-		                document.getElementById("Diagnose").textContent = "Предоставленной информации недостаточно для постановки диагноза типичных форм указанных выше заболеваний, проведите пожалуйста лабораторные анализы";}
+		                document.getElementById("Diagnose").textContent = "Предоставленной информации недостаточно для постановки диагноза типичных форм указанных выше заболеваний, проведите пожалуйста лабораторный анализ";}
 		            }
 
 	}
